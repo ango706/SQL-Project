e280de6a9fbbcbe5833ffbd964a3dff510e1470c
Angela North <141251199+ango706@users.noreply.github.com>
2023-08-10 17:11:13 +0300

    Create TopMoviesbyRevenue.sql

---
+++ TopMoviesbyRevenue.sql
@@ -0,0 +1,12 @@
+SELECT
+    movie_title,
+    SUM(rental_rate) AS total_revenue
+FROM
+    film
+    JOIN inventory ON film.film_id = inventory.film_id
+    JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY
+    movie_title
+ORDER BY
+    total_revenue DESC
+LIMIT 5;
